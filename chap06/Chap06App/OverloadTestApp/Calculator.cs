47d8c6f86e00deaeb8498ccbb763c9e878bf6a77
guemin96 <guemin12@naver.copm>
2021-02-24 11:37:30 +0900

    6장 완료

---
+++ chap06/Chap06App/OverloadTestApp/Calculator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace OverloadTestApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("계산기!");
+
+            int x = Calculator.Plus(3, 4);
+
+            Console.WriteLine($" 3 + 4 = {x}");
+
+
+            float y = Calculator.Plus(3.14f, 5.6f);
+            Console.WriteLine($" 3.14 + 5.6 = {y}");
+
+            Console.WriteLine($"3.14 + 5.6 ={Calculator.Plus(3.14 , 5.6)}"); //실수형 
+
+            Console.WriteLine($"3.14 + 5 ={Calculator.Plus(3.14, "5")}"); //실수형 
+
+            int z = Calculator.Sum(7, 8, 9);
+
+            Console.WriteLine(z);
+
+            int total = 0;
+            total = Sum(1, 2, 3);
+
+            Console.WriteLine($"합계는 {total}");
+
+            Console.WriteLine($"1~10까지의 합은 {Sum(1,2,3,4,5,6,7,8,9)}");
+
+            int[] arry = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine($"1~10까지의 합은 {Sum(arry)}");
+        }
+
+        
+        
+        private static int Sum(params int[] args)//가변길이 매개 함수 
+        {
+            int result = 0;
+            foreach (var arg in args)
+            {
+                result += arg;//result = result + arg;
+
+            }
+            return result;
+        }
+
+        private static double Plus(double v1, string v2)
+        {
+            double.TryParse(v2, out double p2);
+            double result = v1+ p2;
+           
+            
+            return result;
+        }
+
+        private static double Plus(double v1, double v2)
+        {
+            double result = v1 + v2;
+            return result;
+        }
+
+        private static float Plus(float v1, float v2)
+        {
+            float result = v1 + v2;
+            return result;
+        }
+
+        private static int Plus(int p1, int p2)
+        {
+            int result = p1 + p2;
+            return result;
+        }
+    }
+}
--- chap06/Chap06App/profileTestApp/Program.cs
+++ chap06/Chap06App/profileTestApp/Program.cs
@@ -18,9 +18,13 @@ namespace profileTestApp
                 Console.WriteLine("프로필 출력시 오류가 발생했습니다.");
             }
 
+            printProfile(phone:"010-9999-9999",name:"홍길동");
+            printProfile(name: "홍길순", phone: "010-9939-0987");
+            printProfile("박규민");
 
         }
-        public static int printProfile(string name, string phone)
+        public static int printProfile(string name, string phone = "010-7979-7979")// default 파라미터는 뒤에서부터 들어갈 수 있음 지금 경우에 name에는 디폴트값을 넣고 phone에는 넣지 않으면 오류가 남
+            //그렇기 때문에 name에만 디폴트 값을 넣고 싶으면 순서를 바꿔주는게 좋음
         {
             if (string.IsNullOrEmpty(name))
             {
