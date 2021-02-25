79a85e16f4db0d2f1248f994fa7d4032fe554d37
guemin96 <guemin12@naver.copm>
2021-02-25 12:30:48 +0900

    7장 완료

--- chap07/Chap07App/ClassTypeCastApp/Program.cs
+++ chap07/Chap07App/ClassTypeCastApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassTypeCastApp
 {
+    
     class 포유류
     {
         public void 키우다()
@@ -31,7 +32,7 @@ namespace ClassTypeCastApp
     {
         static void Main(string[] args)
         {
-            포유류 _포유류 = new 포유류();
+            포유류 _포유류 = new 포유류();// 클래스의 내용을 가지고 와야되기 때문에
             _포유류.키우다();
             
 
@@ -41,17 +42,17 @@ namespace ClassTypeCastApp
 
             if (_포유류 is 강아지)
             {
-                Console.WriteLine($"쭈쭈도 되나");
-                쭈쭈 = _포유류 as 강아지;
+                Console.WriteLine("쭈쭈도 되나");
+                쭈쭈 = _포유류 as 강아지;// 쭈쭈는 그저 강아지의 지역 변수로만 지정되어 있는 것임, 값을 생성해줘야 함
                 쭈쭈.멍멍();
             }
 
 
 
 
-            강아지 뽀삐 = new 강아지();
-            뽀삐 = null;
-            if (뽀삐 is 포유류)//강아지 -> 포유류   작은것(자식)에서 큰거(부모)으로 가는 것은 안됨.. overriding
+            강아지 뽀삐 = new 강아지(); //강아지 뽀삐라는 지역 변수에 강아지 클래스를 넣어줌
+            뽀삐 = null;// null???? 왜 넣는지 잘 모르겠음.. 그리고 생략하면 왜 오류가 나는 것이고 
+            if (뽀삐 is 포유류)//강아지 -> 포유류   작은것(자식)에서 큰거(부모)으로 가는 것은 안됨.. overriding(값이 넘쳐버리게 됨)
             {
                 Console.WriteLine("이 부분이 실행됩니다.");
                 뽀삐 = new 포유류() as 강아지;
