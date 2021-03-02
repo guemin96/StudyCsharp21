073fb462cfd65a48ab10504ca13ef076cd1b3fba
guemin96 <guemin12@naver.copm>
2021-03-02 10:34:38 +0900

    10장 커밋

---
+++ chap10/Chap10App/ArrayListApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList();
+            array.Add(80);
+            array.Add(74);
+            array.Add(81);
+            array.Add(90);
+            array.Add(34);
+
+            var loc = array.IndexOf(81);
+            array.Insert(loc, 50);
+            array.RemoveAt(loc);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item}");
+
+            }
+
+            array.RemoveAt(4);// 90 삭제
+
+        }
+    }
+}
---
+++ chap10/Chap10App/HashTableApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Hashtable ht = new Hashtable();
+            ht["일"] = "One";
+            ht["이"] = "Two";
+            ht["삼"] = "Three";
+            ht["사"] = "Four";
+            ht["오"] = "Five";
+            //컬럼명 : 값 ,DB랑 연동할 때 많이 사용
+            Console.WriteLine(ht["일"]); // One
+            Console.WriteLine(ht["이"]); // Two
+            Console.WriteLine(ht["사"]); // Four
+            Console.WriteLine(ht["오"]); // Five
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");// 정렬X 키 값
+
+            }
+            
+            int[] arr = { 123, 456, 789 };
+            ArrayList list = new ArrayList(arr);
+            ArrayList list2 = new ArrayList(new int[] { 123, 456, 789 });
+            ArrayList list3 = new ArrayList() { 123, 456, 789 };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            Console.WriteLine($"{list}");
+            Console.WriteLine(list2);
+            Console.WriteLine(list3);
+        }
+    }
+}
