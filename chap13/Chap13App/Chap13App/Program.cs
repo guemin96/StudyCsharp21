b8c570bb6ad5d7aac7ba18fa0afd867cc3e830c3
guemin96 <guemin12@naver.copm>
2021-03-02 17:25:28 +0900

    13장 커밋

---
+++ chap13/Chap13App/Chap13App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap13App
+{
+    delegate int MyDelegate(int a, int b);// 대리자, 대신 호출한다. 대리자와 대리하는 함수의 파라미터의 수는 동일해야한다.
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Calculator calc = new Calculator();
+                MyDelegate Callback;
+
+                Callback = new MyDelegate(calc.Plus);
+                Console.WriteLine($"result ={Callback(3,4)}");
+
+                Callback = new MyDelegate(calc.Minus);
+                Console.WriteLine($"result ={Callback(5, 2)}");
+
+                //var result = calc.Plus(3, 4);
+                //Console.WriteLine($"result ={result}");
+            }
+        }
+    }
+}
---
+++ chap13/Chap13App/DelegateChainApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegateChainApp
+{
+    delegate void AllCalc(int x, int y); // 대리자 선언
+    //이벤트를 처리하기 위해 delegate가 나왔다!!!!!!!!!
+    class Program
+    {
+        static void Plus(int a, int b) { Console.WriteLine($"a+b={a + b}"); }
+        static void Minus(int a, int b) { Console.WriteLine($"a-b={a - b}"); }
+        static void Multiple(int a, int b) { Console.WriteLine($"a*b={a * b}"); }
+        static void Divide(int a, int b) { Console.WriteLine($"a/b={a / b}"); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculating!!");
+            AllCalc allCalc = Plus;
+            allCalc += Minus;
+            allCalc += Multiple;
+            allCalc += Divide;
+
+            allCalc(10, 5); // 하나의 호출로 여러가지 일을 처리할 수 있음 allCalc를 호출함으로써 더하기,빼기,곱하기,나누기를 다 호출할 수 있다.
+
+            Console.WriteLine("곱셈 메서드 제거");
+            allCalc -= Multiple;
+
+            allCalc(6, 5);
+        }
+    }
+}
