6488ec6c8cbc3b5279724f4a525f46281a6320bf
guemin96 <guemin12@naver.copm>
2021-03-04 16:23:38 +0900

    시험문제 커밋

---
+++ chap99/AddressBookApp/AddressBookApp/DataFileManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace AddressBookApp
+{
+    class DataFileManager
+    {
+        string dataFileName = "address.dat";
+        //D:\GitRepository\StudyCsharp21\chap99\AddressBookApp\AddressBookApp\bin\Debug\netcoreapp3.1
+
+        public List<AddressInfo> ReadData()
+        {
+            var listResult = new List<AddressInfo>();
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일
+
+
+            var sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)); //open
+            while (sr.EndOfStream == false)
+            {
+                var temp = sr.ReadLine();
+                //temp 잘라서 manager.listAddess 할당
+                var splits = temp.Split("|");
+                listResult.Add(new AddressInfo() { Name = splits[0], Phone = splits[1], Address = splits[2] });
+            }
+            sr.Close();
+
+            return listResult;
+        }
+        public void WriteData(List<AddressInfo> param)
+        {
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일
+
+            //주소록을 다시 파일에 씀
+            StreamWriter sw = new StreamWriter(
+                new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write));
+            if (param.Count > 0)
+            {
+                foreach (var item in param)
+                {
+                    sw.WriteLine($"{item.Name}|{item.Phone}|{item.Address}");
+                }
+
+            }
+            sw.Close();
+
+        }
+    }
+}
---
+++ chap99/AddressBookApp/AddressBookApp/MainApp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AddressBookApp
+{
+    
+    class MainApp
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                AddressManager manager = new AddressManager();
+                manager.listAddress = new List<AddressInfo>();
+
+                DataFileManager fileManager = new DataFileManager();
+                manager.listAddress = fileManager.ReadData();
+
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine(Environment.CurrentDirectory);
+                    manager.PrintMenu();
+                    int menuNum = manager.SelectMenu();
+
+                    //Console.WriteLine($"선택된 메뉴번호 : {menuNum}");
+
+
+                    switch (menuNum)
+                    {
+                        /*case 0: // 메뉴 선택 실패, 다시 메뉴로 돌아감
+                            break;*/
+                        case 1: // 주소입력 화면 전환
+                            Console.Clear();
+                            manager.InputAddess();
+
+                            break;
+                        case 2: // 주소 검색
+                            Console.Clear();
+                            manager.SearchAddress();
+
+                            break;
+                        case 3: // 주소 수정
+                            Console.Clear();
+                            manager.UpdateAddress();
+
+                            break;
+                        case 4: // 주소 삭제
+                            Console.Clear();
+                            manager.DeleteAddress();
+
+
+                            break;
+                        case 5: // 주소 전체 출력
+                            Console.Clear();
+                            manager.PrintAllAddress();
+
+                            break;
+                        case 6: // 프로그램 종료
+                            fileManager.WriteData(manager.listAddress);
+
+                            Environment.Exit(0);
+                            break;
+                        default: // 0은 여기서 처리, 아무 로직 없음
+                            break;
+                    }
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+            
+        }
+
+        
+        
+
+        
+
+        
+        
+    }
+}
