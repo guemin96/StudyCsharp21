83412e5760b6f8e15167a30e96ae5c2c15dd79d0
guemin96 <guemin12@naver.copm>
2021-03-05 12:41:48 +0900

    19장 스레드

---
+++ chap19/Chap19App/Chap19App/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chap19App
+{
+    class Program
+    {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"DoSomething  :  {i}");
+                //Thread.Sleep(10);// 1/100초 동안 멈춤
+            }
+            
+        }
+        static void SomethingHappen()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"SomethingHappen  :  {i}");
+                Thread.Sleep(10);// 1/100초 동안 멈춤
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Thread thread = new Thread(new ThreadStart(DoSomething));//대리자
+            Thread thread1 = new Thread(SomethingHappen);
+
+            
+           
+            try
+            {
+                Console.WriteLine("스레드 시작");
+                thread.Start();
+                thread1.Start();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine($"Main thread : {i}");
+                    Thread.Sleep(10);
+
+                    if (i == 10)
+                    {
+                        thread.Abort();
+                    }
+                }
+
+
+                Console.WriteLine("스레드 종료 대기");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+
+            }
+            finally
+            {
+                thread.Join();
+                thread1.Join();
+
+                Console.WriteLine("프로세스 종료");
+            }
+        }
+    }
+}
---
+++ chap19/Chap19App/ThreadStateApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStateApp
+{
+    class Program
+    {
+        static void Printstate(ThreadState state)
+        {
+            Console.WriteLine($"{state,-16} : {(int)state}");
+        }
+        static void Main(string[] args)
+        {
+            Printstate(ThreadState.Running);
+
+            Printstate(ThreadState.Aborted);
+            Printstate(ThreadState.StopRequested);
+            Printstate(ThreadState.SuspendRequested);
+            Printstate(ThreadState.Suspended);
+            Printstate(ThreadState.Stopped);
+            Printstate(ThreadState.Unstarted);
+            Printstate(ThreadState.WaitSleepJoin);
+            Printstate(ThreadState.AbortRequested);
+            Printstate(ThreadState.Aborted);
+
+        }
+    }
+}
