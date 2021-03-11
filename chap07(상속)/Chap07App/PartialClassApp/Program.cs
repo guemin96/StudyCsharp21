81a9557d86453ad54d1a1efab983d23a66330b69
박규민 <guemin12@naver.com>
2021-03-11 23:40:13 +0900

    상속

---
+++ chap07(상속)/Chap07App/PartialClassApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassApp
+{
+    partial class MyClass//partial을 사용함으로써 MyClass라는 클래스를 나눠서 저장할 수 있다.
+    {
+        public void Method3() { Console.WriteLine("Method3"); }
+        public void Method4() { Console.WriteLine("Method4"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass _myclass = new MyClass();
+            _myclass.Method1();
+            _myclass.Method2();
+            _myclass.Method3();
+            _myclass.Method4();
+        }
+    }
+}
